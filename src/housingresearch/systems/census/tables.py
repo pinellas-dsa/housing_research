08237d6a4b4a70df5d9b23fc605b4a1a6f9bcad3
James Trimarco <james.trimarco@gmail.com>
2022-04-26 21:45:28 -0400

    Data lookup for a single ACS table

---
+++ src/housingresearch/systems/census/tables.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+acs_tables = {
+    "B25070": {  # lookup code for one table
+        "table_name": "Gross Rent as a Percentage of Household Income in the Past 12 Months",
+        "variables": {
+            "001": {
+                "label": "Total",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "002": {
+                "label": "Less than 10.0 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "003": {
+                "label": "10.0 to 14.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "004": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "005": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "006": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "007": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "008": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "009": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "010": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+            "011": {
+                "label": "15.0 to 19.9 Percent",
+                "value": pd.NA,
+                "error": pd.NA,
+            },
+        },
+    },
+}
